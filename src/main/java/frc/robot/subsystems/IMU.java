949b505519f164f69873915e69c02dad3ea3c8dc
8590_Fan <123890489+ConnorGeraghty1@users.noreply.github.com>
2024-10-28 08:11:45 -0400

    Add ADIS import

--- src/main/java/frc/robot/subsystems/IMU.java
+++ src/main/java/frc/robot/subsystems/IMU.java
@@ -5,6 +5,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.analog.adis16470.frc.ADIS16470_IMU;
 
 public class IMU extends SubsystemBase {
   
