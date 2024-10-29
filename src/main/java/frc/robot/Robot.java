c620f248a9171d802eb56070216dd6c1900bd918
8590_Fan <123890489+ConnorGeraghty1@users.noreply.github.com>
2024-10-29 08:15:53 -0400

    import ADIS

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-
+import com.analog.adis16470.frc.ADIS16470_IMU;
 
 
 /**
