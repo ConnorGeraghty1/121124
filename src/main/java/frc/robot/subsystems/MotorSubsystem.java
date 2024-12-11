5850de2b9bb570a74bb0931926f148dc99ffa4d2
8590_Fan <123890489+ConnorGeraghty1@users.noreply.github.com>
2024-12-11 15:34:24 -0500

    Update MotorSubsystem.java

--- src/main/java/frc/robot/subsystems/MotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/MotorSubsystem.java
@@ -1,5 +1,10 @@
+package frc.robot.subsystems;
+
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 public class MotorSubsystem extends SubsystemBase {
     private final CANSparkMax motor;
@@ -7,10 +12,16 @@ public class MotorSubsystem extends SubsystemBase {
     public MotorSubsystem() {
         // Initialize the SparkMax with the appropriate CAN ID and motor type
         motor = new CANSparkMax(1, MotorType.kBrushless); // Replace 1 with your CAN ID
+        motor.restoreFactoryDefaults();
+        motor.setIdleMode(IdleMode.kBrake);
+        motor.enableVoltageCompensation(11);
+        motor.setSmartCurrentLimit(12);
+        motor.burnFlash();
     }
 
     // Method to set motor speed
     public void setSpeed(double speed) {
+        System.out.println("Setting Motor Speed");
         motor.set(speed);
     }
 
