a63485ccd3c1ea37f0ad748f0687286663fb1de8
8590_Fan <123890489+ConnorGeraghty1@users.noreply.github.com>
2024-12-11 15:33:18 -0500

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,9 @@
+package frc.robot;
+
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.subsystems.MotorSubsystem;
+import frc.robot.commands.RunMotorCommand;
 
 public class RobotContainer {
     private final XboxController controller = new XboxController(0); // Controller on port 0
@@ -11,6 +15,6 @@ public class RobotContainer {
 
     private void configureButtonBindings() {
         new JoystickButton(controller, XboxController.Button.kA.value)
-            .whileHeld(new RunMotorCommand(motorSubsystem));
+            .whileTrue(new RunMotorCommand(motorSubsystem));
     }
 }
